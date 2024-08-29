7bcb4f2626c635e28aeda36d4a41d28bfd99ded6
Yourfirstname Yourlastname <your-email@example.com>
2024-08-29 16:29:26 +1000

    Added form html

--- index.php
+++ index.php
@@ -17,7 +17,52 @@
   include 'navigation.php';
   ?>
   <h1 class="title">Create an account</h1>
+  <main class="container-registration">
+    <form action="" method="post">
+      <label for="username">Username:
+        <input type="text" id="username" name="userName" value="">
+      </label>
+      <br>
+      <label for="firstname">First Name:
+        <input type="text" id="firstname" name="firstName" value="">
+      </label>
+      <br>
+      <label for="surname">Surname:
+        <input type="text" id="surname" name="surName" value="">
+      </label>
+      <br>
+      <label for="password">Password:
+        <input type="text" id="password" name="passWord" value="">
+      </label>
+      <br>
+      <label for="address">Address:
+        <input type="text" id="address" name="adDress" value="">
+      </label>
+      <br>
+      <label for="suburb">Suburb:
+        <input type="text" id="suburb" name="suBurb" value="">
+      </label>
+      <br>
+      <label for="postcode">Postcode:
+        <input type="number" id="suburb" name="suBurb" value="">
+      </label>
+      <br>
+      <label for="state">State:
+        <input type="text" id="state" name="sTate" value="">
+      </label>
+      <br>
+      <label for="mobilephone">Mobilephone:
+        <input type="text" id="mobilephone" name="mobilephone" value="">
+      </label>
+      <br>
+      <input type="submit" name="Create New Account" value="Create New Account">
+      <br>
+      <input type="submit" name="Check Details" value="Check Details">
 
+      <input type="submit" name="Update Details" value="Update Details">
+      <input type="submit" name="DeleteAccount" value="Delete Account">
+    </form>
+  </main>
 
   <?php
   include 'footer.php';
