9a6e8eb19a462e126877e1ee061fe0f94ab04cc2
Yourfirstname Yourlastname <your-email@example.com>
2024-08-29 12:17:08 +1000

    Set up folder and file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registration form | Computer Force</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
