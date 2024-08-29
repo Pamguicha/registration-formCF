a520ef7449aad698c278019c71708bb465784eff
Yourfirstname Yourlastname <your-email@example.com>
2024-08-29 15:41:10 +1000

    Added navigation bar, footer and style

--- index.php
+++ index.php
@@ -4,11 +4,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
   <title>Registration form | Computer Force</title>
 </head>
 
 <body>
+  <img class="logo" src="images/logoCf.png" alt="an image of the logo of Computer Force">
+  <?php
+  include 'navigation.php';
+  ?>
+  <h1 class="title">Create an account</h1>
 
+
+  <?php
+  include 'footer.php';
+  ?>
 </body>
 
 </html>
---
+++ navigation.php
@@ -0,0 +1,24 @@
+<nav class="nav_bar">
+  <ul class="menuList">
+    <li class="menuListLink">
+      <a class="menuClickeableLink homeLink" href="index.php">Home</a>
+    </li>
+    <li class="menuListLink">
+      <a class="menuClickeableLink" href="products.php">Products</a>
+    </li>
+    <li class="menuListLink">
+      <a class="menuClickeableLink" href="register.php">Register</a>
+    </li>
+    <li class="menuListLink">
+      <a class="menuClickeableLink" href="cart.php">Cart</a>
+    </li>
+    <li class="menuListLink">
+      <a class="menuClickeableLink" href="about.php">About</a>
+    </li>
+  </ul>
+  <form role="search" class="search-container">
+    <input class="search-bar" type="search" aria-label="search results box" placeholder="search brands" id="search"
+      value="">
+    <input class="search-btn" type="button" value="search" aria-label="submision button">
+  </form>
+</nav>
