374b8c515aa688d534f81d9dbaa1a97c294ef734
Yourfirstname Yourlastname <your-email@example.com>
2024-08-30 10:46:38 +1000

    Added style to the registration form

--- index.php
+++ index.php
@@ -18,44 +18,53 @@
   ?>
   <h1 class="title">Create an account</h1>
   <main class="container-registration">
-    <form action="" method="post">
-      <label for="username">Username:
-        <input type="text" id="username" name="userName" value="">
+    <form class="form-container" action="" method="post">
+      <label class="userClass" for="username">Username:
+        <br>
+        <input class="inputRegForm" type="text" id="username" name="userName" value="">
       </label>
       <br>
-      <label for="firstname">First Name:
-        <input type="text" id="firstname" name="firstName" value="">
+      <label class="nameClass" for="firstname">First Name:
+        <br>
+        <input class="inputRegForm" type="text" id="firstname" name="firstName" value="">
       </label>
       <br>
-      <label for="surname">Surname:
-        <input type="text" id="surname" name="surName" value="">
+      <label class="surnameClass" for="surname">Surname:
+        <br>
+        <input class="inputRegForm" type="text" id="surname" name="surName" value="">
       </label>
       <br>
-      <label for="password">Password:
-        <input type="text" id="password" name="passWord" value="">
+      <label class="passwordClass" for="password">Password:
+        <br>
+        <input class="inputRegForm" type="text" id="password" name="passWord" value="">
       </label>
       <br>
-      <label for="address">Address:
-        <input type="text" id="address" name="adDress" value="">
+      <label class="addressClass" for="address">Address:
+        <br>
+        <input class="inputRegForm" type="text" id="address" name="adDress" value="">
       </label>
       <br>
-      <label for="suburb">Suburb:
-        <input type="text" id="suburb" name="suBurb" value="">
+      <label class="suburbClass" for="suburb">Suburb:
+        <br>
+        <input class="inputRegForm" type="text" id="suburb" name="suBurb" value="">
       </label>
       <br>
-      <label for="postcode">Postcode:
-        <input type="number" id="suburb" name="suBurb" value="">
+      <label class="postcodeClass" for="postcode">Postcode:
+        <br>
+        <input class="inputRegForm" type="number" id="suburb" name="suBurb" value="">
       </label>
       <br>
-      <label for="state">State:
-        <input type="text" id="state" name="sTate" value="">
+      <label class="stateClass" for="state">State:
+        <br>
+        <input class="inputRegForm" type="text" id="state" name="sTate" value="">
       </label>
       <br>
-      <label for="mobilephone">Mobilephone:
-        <input type="text" id="mobilephone" name="mobilephone" value="">
+      <label class="mobileClass" for="mobilephone">Mobilephone:
+        <br>
+        <input class="inputRegForm" type="text" id="mobilephone" name="mobilephone" value="">
       </label>
       <br>
-      <input type="submit" name="Create New Account" value="Create New Account">
+      <input class="newAccount" type="submit" name="Create New Account" value="Create New Account">
       <br>
       <input type="submit" name="Check Details" value="Check Details">
 
